015b646262ab89f88ff5566fe95c2a08b4301be7
LeoGomes0919 <leonardogs0919@gmail.com>
2021-03-18 20:23:30 -0300

    Conclusão do desafio.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,19 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+interface ContentProps {
+  title: string;
+  poster: string;
+  rating: string;
+  runtime: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <MovieCard
+      title={props.title}
+      poster={props.poster}
+      runtime={props.runtime}
+      rating={props.rating}
+    />
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,21 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+interface SideBarProps {
+  handleClickButton(id: number): void;
+  id: number;
+  title: string;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  selectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <Button
+      id={String(props.id)}
+      title={props.title}
+      iconName={props.name}
+      onClick={() => props.handleClickButton(props.id)}
+      selected={props.selectedGenreId === props.id}
+    />
+  );
 }
